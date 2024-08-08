efb1c02af834d5d9c0d91d4244af55945f4dcbce
Yonatan <yonatanforsh@gmail.com>
2024-08-08 09:27:40 +0300

    send2

---
+++ JsonController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+namespace AttackServer
+{
+    internal class JsonController
+    {
+        public static List<Missile> ReadJson(string JsonPath)
+        {
+            string text = File.ReadAllText(JsonPath);
+            List<Missile> MIssileList = JsonSerializer.Deserialize< List < Missile >>(text);
+            return MIssileList;
+        }
+    }
+}
---
+++ Missile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttackServer
+{
+    internal class Missile
+    {
+        public Missile() { }
+
+        public string Name { get; set; }
+        public double Speed { get; set; }
+        public double Mass { get; set; }
+        public double Time { get; set; }
+        public Dictionary<string, int> Origin { get; set; }
+        public Dictionary<string, int> Angle { get; set; }
+        public int Demage { get; set; }
+
+    }
+}
